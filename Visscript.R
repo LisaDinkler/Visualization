2acf29874f78c66f462724aad142169560beb106
LisaDinkler <42734983+LisaDinkler@users.noreply.github.com>
2019-07-08 15:05:01 +0200

    Add files via upload

---
+++ Visscript.R
@@ -0,0 +1,9 @@
+full_trains <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-26/full_trains.csv")
+
+names(full_trains)
+
+require(ggplot2)
+require(dplyr)
+
+ggplot (full_trains, aes(x = year, y = total_num_trips)) +
+  geom_jitter(alpha = .25, position = position_jitter(0.05))
